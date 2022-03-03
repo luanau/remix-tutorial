1302fcf36400cbe6b8865165b9a910f4e44c447d
luanau <luanau@gmail.com>
2022-03-03 17:01:07 +0000

    login done

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -8,6 +8,7 @@ import {
 
 import { db } from "~/utils/db.server";
 import stylesUrl from "~/styles/login.css";
+import { createUserSession, login } from "~/utils/session.server";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
@@ -73,10 +74,17 @@ export const action: ActionFunction = async ({
       // login to get the user
       // if there's no user, return the fields and a formError
       // if there is a user, create their session and redirect to /jokes
-      return badRequest({
-        fields,
-        formError: "Not implemented",
-      });
+      let user = await login({username, password});
+      console.log("user=",user)
+      if (!user) {
+          return {
+              formError: `Invalid username or password`,
+              fields,
+          }
+      }
+
+      // if there is a user, create their session and redirect to /jokes
+      return createUserSession(user.id, redirectTo)
     }
     case "register": {
       const userExists = await db.user.findFirst({
