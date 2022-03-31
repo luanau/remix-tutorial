2810e8ecd18939351a6858b7f80aa2993723207a
luanau <luanau@gmail.com>
2022-03-31 12:49:51 +0100

    meta and seo

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,17 +1,33 @@
-import type { ActionFunction, LinksFunction } from "remix";
+import type {
+  ActionFunction,
+  LinksFunction,
+  MetaFunction,
+} from "remix";
 import {
   useActionData,
   json,
-  Link,
   useSearchParams,
+  Link,
 } from "remix";
 
 import { db } from "~/utils/db.server";
+import {
+  createUserSession,
+  login,
+  register,
+} from "~/utils/session.server";
 import stylesUrl from "~/styles/login.css";
-import { createUserSession, login, register } from "~/utils/session.server";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export const meta: MetaFunction = () => {
+  return {
+    title: "Remix Jokes | Login",
+    description:
+      "Login to submit your own jokes to Remix Jokes!",
+  };
 };
 
 function validateUsername(username: unknown) {
@@ -71,20 +87,14 @@ export const action: ActionFunction = async ({
 
   switch (loginType) {
     case "login": {
-      // login to get the user
-      // if there's no user, return the fields and a formError
-      // if there is a user, create their session and redirect to /jokes
-      let user = await login({username, password});
-      console.log("user=",user)
+      const user = await login({ username, password });
       if (!user) {
-          return {
-              formError: `Invalid username or password`,
-              fields,
-          }
+        return badRequest({
+          fields,
+          formError: `Username/Password combination is incorrect`,
+        });
       }
-
-      // if there is a user, create their session and redirect to /jokes
-      return createUserSession(user.id, redirectTo)
+      return createUserSession(user.id, redirectTo);
     }
     case "register": {
       const userExists = await db.user.findFirst({
@@ -96,10 +106,14 @@ export const action: ActionFunction = async ({
           formError: `User with username ${username} already exists`,
         });
       }
-      // create the user
-      let user = await register({username, password});
-      // create their session and redirect to /jokes
-      return createUserSession(user.id, redirectTo)
+      const user = await register({ username, password });
+      if (!user) {
+        return badRequest({
+          fields,
+          formError: `Something went wrong trying to create a new user.`,
+        });
+      }
+      return createUserSession(user.id, redirectTo);
     }
     default: {
       return badRequest({
